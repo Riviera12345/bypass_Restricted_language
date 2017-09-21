c0aab20e25cc856130900bf460c5cc9997674888
Matt Homjxi0e  _ <jihadLkmaty218@users.noreply.github.com>
2017-09-21 17:11:49 +0200

    Add files via upload

---
+++ bypassing.ps1
@@ -0,0 +1,11 @@
+
+Set-ExecutionPolicy RemoteSigned -Scope Process
+
+      Set-ExecutionPolicy RemoteSigned -Scope Process -Force -Verbose | Invoke-AsWorkflow  -CommandName Path File , Exploit or Code !1
+ "complete Bypass"
+
+sleep 3.22
+
+  exit
+
+sleep 2
